d8e9b58b3e4923730107499e030ea925243ff3ff
Hahahahihihohe <94295754+Hahahahihihohe@users.noreply.github.com>
2024-05-13 14:38:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ def backend():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("hello world")
+    print("cute")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
