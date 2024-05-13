7fa59a9379f91bc149ff1bd92f686ed2d7f9f814
Đình Hiếu <hieund.vtv@gmail.com>
2024-05-13 09:32:44 +0700

    first test

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("code day ne")
+    print("kho noi")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
