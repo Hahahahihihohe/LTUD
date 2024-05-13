df7734d7a34605715dd8fa692abf96e94229b62e
Đình Hiếu <hieund.vtv@gmail.com>
2024-05-13 14:34:30 +0700

    hello

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("kho noi")
+    print("hello world")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
