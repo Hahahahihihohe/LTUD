8feab9e0954503a05f9c2f929cc51d3aa88dadc6
Đình Hiếu <hieund.vtv@gmail.com>
2024-05-14 21:26:36 +0700

    cập nhật class login, chỉnh sửa hàm đổi mật khẩu và tạo tài khoản mới

--- login.py
+++ login.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Login():
     def __init__(self,database):
         self.user = None
@@ -13,7 +15,7 @@ class Login():
             else:
                 return False
 
-    def forgot_pass(self,user):
+    def forgot_pass(self,user,password):
         # Hàm xử lý để đặt lại mật khẩu, trước mắt yêu cầu điền username
         index = -1
         for i in range(self.database.shape[0]):
@@ -21,16 +23,67 @@ class Login():
                 index = i
                 break
         if index == -1:
-            return "tai khoan khong ton tai"
-        else:
-            flag = False
-            while ~flag:
-                flag = True
-                new_pass = input()
-                if len(new_pass) < 8:
-                    #kiem tra dieu kien do dai
-                    flag = False
-                # se them mot vai dieu kien khac sau
-            self.database[index][1] = new_pass
-            return "sua mat khau thanh cong"
+            #nếu tài khoản không tồn tại thì không thể đổi mật khẩu
+            print("tai khoan khong ton tai")
+            return False
 
+        #điều kiện mật khẩu gồm có: có ít nhất 8 kí tự
+        #                           có ít nhất 1 ký tự viết hoa
+        #                           có ít nhất 1 chữ số
+
+        if len(password) < 8:
+            print("Mật khẩu phải có ít nhất 8 ký tự")
+            return False
+        num = False
+        cap = False
+        for i in password:
+            if i.isdigit():
+                num = True
+            if i.isupper():
+                cap = True
+        if not num:
+            print("Mật khẩu phải có ít nhất 1 chữ số")
+            return False
+        if not cap:
+            print("Mật khẩu phải có ít nhất 1 ký tự viết hoa")
+            return False
+        self.database[index][1] = password
+        return True
+
+
+    def create_acc(self,user,password):
+        index = -1
+        for i in range(self.database.shape[0]):
+            if self.database[i][0] == user:
+                index = i
+                break
+
+        #nếu đã tồn tại không thể thêm vào
+        if index != -1:
+            print("tài khoản này đã tồn tại")
+            return False
+
+        # điều kiện mật khẩu gồm có: có ít nhất 8 kí tự
+        #                           có ít nhất 1 ký tự viết hoa
+        #                           có ít nhất 1 chữ số
+        if len(password) < 8:
+            print("Mật khẩu phải có ít nhất 8 ký tự")
+            return False
+        num = False
+        cap = False
+        for i in password:
+            if i.isdigit():
+                num = True
+            if i.isupper():
+                cap = True
+        if not num:
+            print("Mật khẩu phải có ít nhất 1 chữ số")
+            return False
+        if not cap:
+            print("Mật khẩu phải có ít nhất 1 ký tự viết hoa")
+            return False
+        self.database = np.vstack((self.database,[user,password]))
+        return True
+
+    def print_database(self):
+        print(self.database)
