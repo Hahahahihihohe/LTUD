a5e507b86d02922b2f6f1cdc1f4598a87357201e
Đình Hiếu <hieund.vtv@gmail.com>
2024-05-15 21:13:50 +0700

    cập nhật database = sql, chỉnh sửa phương thức lấy dữ liệu class login

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from login import Login
+import mysql.connector
 
-def backend():
-    print("hola")
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print("hello world")
-    print("cute")
-    print("3hieu")
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
--- user.py
+++ user.py
@@ -7,34 +7,3 @@ class user():
         self.gender = gender
         self.money = money
 
-    def set_name(self,name):
-        #set name
-        self.name = name
-
-    def get_name(self):
-        #get name
-        return self.name
-
-    def set_gender(self, gender):
-        # set giới tính
-        self.gender = gender
-
-    def get_gender(self):
-        # get gioi tinh
-        return self.gender
-
-    def set_age(self, age):
-        # set tuoi
-        self.age = age
-
-    def get_age(self):
-        # get age
-        return self.age
-
-    def set_money(self, money):
-        # set money
-        self.money = money
-
-    def get_money(self):
-        # get name
-        return self.money
