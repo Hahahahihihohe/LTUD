413e7a3b46c4e09f693beca3755d7934e9057dc2
Đình Hiếu <hieund.vtv@gmail.com>
2024-05-21 21:28:49 +0700

    update database

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import mysql.connector
 from database import db
 import numpy as np
 from user import User
-mydb = mysql.connector.connect(host="moviedatabase-moviedatabase.e.aivencloud.com"
-                                       , user="avnadmin"
-                                       , password="AVNS_hkSwZWZPqV6SHYraARW"
-                                       , port=14802,
-                                       database='client')
-
-
-mycursor = mydb.cursor()
-mycursor.execute("describe movie_time")
-res = np.array(mycursor.fetchall())
+sql = """
+SHOW COLUMNS FROM movie;
+"""
+res = np.array(db.fetchall(sql))
 print(res)
+
+
 
 
 
