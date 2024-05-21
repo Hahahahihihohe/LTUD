cbd8d5da5155b97863e22bf93d8d718d01c9fa3d
Đinh Hiếu <dinhviethieu@gmail.com>
2024-05-21 16:48:32 +0700

    Merge remote-tracking branch 'origin/backend'
    # Conflicts:
    #	main.py

--- front_end.py
+++ front_end.py
@@ -10,8 +10,9 @@ class login_wd(QMainWindow):
         super(login_wd,self).__init__()
         loadUi("login_wd.ui",self)
         self.login = Login()
-        self.register_0.clicked.connect(self.switch)
+        self.register_0.clicked.connect(self.switch_register)
         self.login_btn.clicked.connect(self.check)
+        self.forgot_pw_btn.clicked.connect(self.switch_forgotpw)
 
     #kiểm tra thông tin đăng nhập
     def check(self):
@@ -25,10 +26,14 @@ class login_wd(QMainWindow):
         else:
             QMessageBox.information(self, "Thông báo", "Sai tài khoản hoặc mật khẩu")
     #chuyển sang cửa sổ đăng ký
-    def switch(self):
+    def switch_register(self):
         register_f = register_wd()
         widget.addWidget(register_f)
         widget.setCurrentIndex(1)
+    def switch_forgotpw(self):
+        forgot_f = forgot_pw()
+        widget.addWidget(forgot_f)
+        widget.setCurrentIndex(3)
 #Cửa sổ đăng ký
 class register_wd(QMainWindow):
     def __init__(self):
@@ -62,15 +67,42 @@ class test(QMainWindow):
         super(test,self).__init__()
         loadUi("test.ui",self)
 
+class forgot_pw(QMainWindow):
+    def __init__(self):
+        self.login = Login()
+        super(forgot_pw,self).__init__()
+        loadUi("forgot_pw.ui",self)
+        self.change_pw.clicked.connect(self.check_fgp)
+
+    def check_fgp(self):
+        taikhoan = self.User_old.text()
+        mk_moi = self.pw_new.text()
+        kt_mk_moi = self.pw_new2.text()
+        kt3 = self.login.forgot_pass(taikhoan,mk_moi,kt_mk_moi)
+        if kt3 == 1:
+            QMessageBox.information(self, "Thông báo", "Tài khoản không tồn tại")
+        elif kt3 == 2:
+            QMessageBox.information(self, "Thông báo", "Mật khẩu không hợp lệ (mật khẩu của bạn phải có ít nhất 8 ký tự bao gồm ít nhất 1 chữ số và 1 chữ cái in hoa)")
+        elif kt3 == 3:
+            QMessageBox.information(self, "Thông báo", "Mật khẩu không hợp lệ (mật khẩu của bạn phải có ít nhất 8 ký tự bao gồm ít nhất 1 chữ số và 1 chữ cái in hoa)")
+        elif kt3 == 4:
+            QMessageBox.information(self, "Thông báo", "Mật khẩu không hợp lệ (mật khẩu của bạn phải có ít nhất 8 ký tự bao gồm ít nhất 1 chữ số và 1 chữ cái in hoa)")
+        elif kt3 == 5:
+            QMessageBox.information(self, "Thông báo", "Mật khẩu nhập lại không đúng")
+        elif kt3 == 6:
+            QMessageBox.information(self, "Thông báo", "Đổi mật khẩu thành công")
+            widget.setCurrentIndex(0)
 app = QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
 
 login_f = login_wd()
 register_f = register_wd()
 test_f = test()
+forgot_f = forgot_pw()
 widget.addWidget(login_f)
 widget.addWidget(register_f)
 widget.addWidget(test_f)
+widget.addWidget(forgot_f)
 widget.setCurrentIndex(0)
 widget.setFixedWidth(500)
 widget.setFixedHeight(600)
