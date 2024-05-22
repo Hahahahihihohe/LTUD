cbd91d6b93e79110bb780822a895c3c352f91f5d
Đình Hiếu <hieund.vtv@gmail.com>
2024-05-22 16:40:06 +0700

    update database

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import mysql.connector
 from database import db
 import numpy as np
 from user import User
+from main_scr import Mainscreen,Movie
+
 sql = """
-SHOW COLUMNS FROM movie;
+Select ID, TENPHIM, THOILUONG, NOIDUNG, THELOAI, TUOI from movie
 """
 res = np.array(db.fetchall(sql))
 print(res)
-
+# main_screen = Mainscreen()
+# movie = main_screen.pick_movie(4)
+# print(movie.GetName())
 
 
 
--- main_scr.py
+++ main_scr.py
@@ -1,29 +1,89 @@
 from database import db
+import os
 import numpy as np
 class Movie():
-    def __init__(self, name = None, type = None, duration = None, actor = None, age_res = None, des = None, cover = None):
+    def __init__(self, id = None, name = None, time = None, des = None, type = None, age_res = None):
+        self.id = id
         self.name = name
-        self.type = type
-        self.duration = duration
+        self.time = time
         self.des = des
+        self.type = type
         self.age_res = age_res
-        self.actor = actor
-        self.cover = cover
+        self.cover = None
+
+    #một vài hàm để lấy giá trị mong muốn
+    def GetId(self):
+        #Trả về id
+        return self.id
+
+    def GetName(self):
+        #trả về tên phim
+        return self.name
+
+    def time(self):
+        #Trả về thời lượng phim
+        return self.time
+
+    def GetDes(self):
+        #Trả về mô tả phim
+        return self.des
+
+    def GetType(self):
+        #Trả về thể loại phim
+        return self.type
+
+    def getAge_Res(self):
+        # Trả về giới hạn độ tuổi
+        return self.age_res
 
 class Mainscreen():
     def __init__(self):
-        self.Movie = np.array([])
+        self.Movie = []
+        self.load_movie()
 
     def load_movie(self):
-        sql = "Select * from movie"
+        #load tất cả phim, self.Movie là một list
+        sql = "Select ID, TENPHIM, THOILUONG, NOIDUNG, THELOAI, TUOI from movie"
         res = np.array(db.fetchall(sql))
+        print(res.shape)
         for i in res:
-            self.Movie = np.append(self.Movie, Movie(name = i[1]
-                                                     ,type = i[2]
-                                                     ,duration=i[3]
-                                                     ,actor=i[4]
-                                                     ,age_res=i[5]
-                                                     ,des=i[6]
-                                                     ,cover=i[7]))
+            id , name, time, des, type, age_res = i
+            movie = Movie(id,name, time, des, type,age_res)
+            movie.cover = f"{movie.name}.png"
+            self.Movie.append(movie)
+
+    def pick_movie(self,id):
+        #trả về đối tượng có id mong muốn
+        if id > len(self.Movie):
+            print("Lỗi ID")
+            return 0
+        return self.Movie[id-1]
+
+    def load_cover(self,id):
+        #load anh cover, ten duong dan la tenphim.png
+        sql = """
+        SELECT TENPHIM, image FROM movie WHERE id = %s
+        """
+        res = db.fetchone(sql, [id])
+        name, binary_data = res
+        file_path = f"{name}.png"
+        if os.path.exists(file_path):
+            #neu da tai roi thi thoi
+            print("thu muc da ton tai")
+        else:
+            #chua thi tai ve
+            with open(file_path, 'wb') as file:
+                file.write(binary_data)
+            print(f"Image {name} retrieved and saved")
+
+    def load_all_cover(self):
+        #load het tat ca anh
+        for i in range(len(self.Movie)):
+            self.load_cover(i+1)
+
+
+
+
+
 
 
